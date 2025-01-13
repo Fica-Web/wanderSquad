6e920278e97fea642a7db28a41d101bdaf46381e
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2025-01-13 14:52:37 +0530

    zoom in animation in home Page hero section

--- frontend/src/components/homePage/Hero.jsx
+++ frontend/src/components/homePage/Hero.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import hero from "../../assets/images/hero1.jpg";
-import ReusableButton from '../reusable/ReusableButton';
+import ReusableButton from "../reusable/ReusableButton";
 
 const Hero = () => {
     return (
-        <div
-            className="relative bg-cover bg-center bg-no-repeat bg-fixed h-screen flex items-center justify-center"
-            style={{
-                backgroundImage: `url(${hero})`,
-            }}
-        >
+        <div className="relative h-screen flex items-center justify-center">
+            {/* Background with Zoom Animation */}
+            <div
+                className="absolute inset-0 bg-cover bg-center bg-no-repeat bg-fixed h-full w-full"
+                style={{
+                    backgroundImage: `url(${hero})`,
+                    animation: "infiniteZoom 20s infinite",
+                }}
+            ></div>
+
+            <style>
+                {`
+                @keyframes infiniteZoom {
+                    0% {
+                    transform: scale(1);
+                    }
+                    50% {
+                    transform: scale(1.1);
+                    }
+                    100% {
+                    transform: scale(1);
+                    }
+                }
+            `}
+            </style>
+
             {/* Overlay */}
-            <div className="absolute inset-0 bg-primary bg-opacity-50"></div>
+            <div className="absolute inset-0 bg-primary bg-opacity-50 pointer-events-none"></div>
 
             {/* Content */}
             <div className="relative text-center text-white px-4">
@@ -21,9 +41,9 @@ const Hero = () => {
                 <h3 className="text-lg md:text-xl mb-6">
                     Discover amazing packages, services, and more with just one click.
                 </h3>
-                <ReusableButton 
-                    content={'Discover now'}
-                    color={'border-secondary text-secondary'}
+                <ReusableButton
+                    content={"Discover now"}
+                    color={"border-secondary text-secondary"}
                 />
             </div>
         </div>
